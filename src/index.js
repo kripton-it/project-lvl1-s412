b0033fae6869ad2e7394d4b22fd3cc5500ac808c
kripton-it <kriptonit1988@gmail.com>
2019-01-15 18:40:53 +0500

    created greeting function

---
+++ src/index.js
@@ -0,0 +1,8 @@
+// import readlineSync from 'readline-sync';
+
+export default () => {
+  // const userName = readlineSync.question('May I have your name? ');
+  // console.log(`Hello, ${userName}!`);
+
+  console.log('Hello!');
+};
