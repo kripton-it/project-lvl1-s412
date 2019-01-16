222a36eaa193c0dc5c71310dc501eac936ba5a92
kripton-it <kriptonit1988@gmail.com>
2019-01-16 22:13:05 +0500

    fixed comments to step 4

--- src/even.js
+++ src/even.js
@@ -1,27 +1,13 @@
 import readlineSync from 'readline-sync';
 import { getUserName } from './index';
-import { getRandomInteger, isEven } from './help';
+import getRandomInteger from './help';
 
 const minNumber = 1;
 const maxNumber = 200;
 const maxQuestionsNumber = 3;
 const description = 'Answer "yes" if number even otherwise answer "no"';
 
-const getQuestion = () => getRandomInteger(minNumber, maxNumber);
-
-const getAnswer = () => readlineSync.question('Your answer: ');
-
-const checkAnswer = (question, answer) => {
-  const correctAnswer = isEven(question) ? 'yes' : 'no';
-
-  if (correctAnswer === answer) {
-    console.log('Correct!');
-    return true;
-  }
-
-  console.log(`'${answer}' is wrong answer ;(. Correct answer was '${correctAnswer}'`);
-  return false;
-};
+const isEven = number => number % 2 === 0;
 
 export default () => {
   console.log('Welcome to the Brain Games!');
@@ -29,13 +15,18 @@ export default () => {
   const userName = getUserName();
 
   for (let score = 0; score < maxQuestionsNumber; score += 1) {
-    const question = getQuestion();
+    const question = getRandomInteger(minNumber, maxNumber);
     console.log(`Question: ${question}`);
-    const userAnswer = getAnswer();
-    if (checkAnswer(question, userAnswer) === false) {
-      console.log(`Let's try again, ${userName}!`);
-      return;
+    const correctAnswer = isEven(question) ? 'yes' : 'no';
+    const userAnswer = readlineSync.question('Your answer: ');
+
+    if (correctAnswer === userAnswer) {
+      console.log('Correct!');
     }
+
+    console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'`);
+    console.log(`Let's try again, ${userName}!`);
+    return;
   }
 
   console.log(`Congratulations, ${userName}!`);
--- src/help.js
+++ src/help.js
@@ -1,6 +1,4 @@
-export const getRandomInteger = (min, max) => {
+export default (min, max) => {
   const randomInteger = Math.round(min - 0.5 + Math.random() * (max - min + 1));
   return randomInteger;
 };
-
-export const isEven = number => number % 2 === 0;
