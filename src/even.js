e364aa32fc56132ca75519529e9e3dbb94033bb7
kripton-it <kriptonit1988@gmail.com>
2019-01-16 16:02:25 +0500

    fixed comments for even game

--- src/even.js
+++ src/even.js
@@ -1,65 +1,42 @@
 import readlineSync from 'readline-sync';
 import { getUserName } from './index';
-import { getRandomInteger, printRules } from './help';
+import { getRandomInteger, isEven } from './help';
 
 const minNumber = 1;
 const maxNumber = 200;
 const maxQuestionsNumber = 3;
-const rules = 'Answer "yes" if number even otherwise answer "no"';
-
-const isEven = number => !(number % 2);
+const description = 'Answer "yes" if number even otherwise answer "no"';
 
 const getQuestion = () => getRandomInteger(minNumber, maxNumber);
 
 const getAnswer = () => readlineSync.question('Your answer: ');
 
 const checkAnswer = (question, answer) => {
-  const correctMessage = 'Correct';
-  const wrongMessage = 'Wrong';
-  const wrongYesMessage = '\'yes\' is wrong answer ;(. Correct answer was \'no\'';
-  const wrongNoMessage = '\'no\' is wrong answer ;(. Correct answer was \'yes\'';
+  const correctAnswer = isEven(question) ? 'yes' : 'no';
 
-  const isEvenQuestion = isEven(question);
-
-  switch (answer) {
-    case 'yes':
-      if (!isEvenQuestion) {
-        console.log(wrongYesMessage);
-        return false;
-      }
-      console.log(correctMessage);
-      return true;
-    case 'no':
-      if (isEvenQuestion) {
-        console.log(wrongNoMessage);
-        return false;
-      }
-      console.log(correctMessage);
-      return true;
-    default:
-      console.log(wrongMessage);
-      return false;
+  if (correctAnswer === answer) {
+    console.log('Correct!');
+    return true;
   }
+
+  console.log(`'${answer}' is wrong answer ;(. Correct answer was '${correctAnswer}'`);
+  return false;
 };
 
 export default () => {
   console.log('Welcome to the Brain Games!');
-  printRules(`${rules}\n`);
+  console.log(`${description}\n`);
   const userName = getUserName();
-  let score = 0;
-  while (score < maxQuestionsNumber && score > 0) {
+
+  for (let score = 0; score < maxQuestionsNumber; score += 1) {
     const question = getQuestion();
     console.log(`Question: ${question}`);
-    const answer = getAnswer();
-    if (checkAnswer(question, answer)) {
-      score += 1;
-    } else {
-      score = -1;
+    const userAnswer = getAnswer();
+    if (checkAnswer(question, userAnswer) === false) {
       console.log(`Let's try again, ${userName}!`);
+      return;
     }
   }
 
-  if (score === maxQuestionsNumber) {
-    console.log(`Congratulations, ${userName}!`);
-  }
+  console.log(`Congratulations, ${userName}!`);
 };
--- src/help.js
+++ src/help.js
@@ -1,8 +1,6 @@
-export const printRules = (description) => {
-  console.log(description);
-};
-
 export const getRandomInteger = (min, max) => {
   const randomInteger = Math.round(min - 0.5 + Math.random() * (max - min + 1));
   return randomInteger;
 };
+
+export const isEven = number => number % 2 === 0;
