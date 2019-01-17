00123378da9f6a3b953aa37a7cefe23a47f98446
kripton-it <kriptonit1988@gmail.com>
2019-01-17 22:52:43 +0500

    published version 1.2.0

--- src/games/even.js
+++ src/games/even.js
@@ -1,5 +1,5 @@
 import { play } from '../index';
-import { getRandomInteger } from '../utils';
+import getRandomInteger from '../utils';
 
 const minNumber = 1;
 const maxNumber = 20;
@@ -8,16 +8,15 @@ const description = 'Answer "yes" if number even otherwise answer "no"';
 
 const isEven = number => number % 2 === 0;
 
-const calcAnswer = number => isEven(number) ? 'yes' : 'no';
-
-const askQuestion = () => {
+const getGameData = () => {
   const question = getRandomInteger(minNumber, maxNumber);
+  const correctAnswer = isEven(question) ? 'yes' : 'no';
   return {
-    string: `${question}`,
-    answer: calcAnswer(question),
+    question: `${question}`,
+    answer: correctAnswer,
   };
 };
 
 export default () => {
-  play(description, askQuestion);
+  play(description, getGameData);
 };
